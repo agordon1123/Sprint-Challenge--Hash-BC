06a87951a9d1084e590a105b5b0993ec0420a3ef
Alex <agordon1123@gmail.com>
2020-01-17 13:46:10 -0500

    completed ex2 hashtable

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,17 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
+    # add tickets to hashtable
+    for i in range(length):
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+
+    # find first
+    iterfirst = hash_table_retrieve(hashtable, "NONE")
+    
+    for j in range(length):
+        route[j] = iterfirst
+        # find where key is destination
+        target = hash_table_retrieve(hashtable, iterfirst)
+        iterfirst = target
+
     return route
